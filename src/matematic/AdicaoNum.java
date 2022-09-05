a1cf788cadb1a7762d994c353766e481df0f69c4
Danilo Nunes de Araújo <95918557+daniloaraujodev@users.noreply.github.com>
2022-09-05 15:00:09 -0300

    Curso JAVA

---
+++ src/matematic/AdicaoNum.java
@@ -0,0 +1,26 @@
+package matematic;
+
+import java.util.Scanner;
+
+public class AdicaoNum {
+
+		public static void main(String[] args) {
+				Scanner dados = new Scanner (System.in);
+				System.out.print("Digite o primeiro numero: ");
+				int num1 = dados.nextInt();
+				
+				System.out.print("Digite o segundo numero: ");
+				int num2 = dados.nextInt();
+				
+				System.out.print("Digite o terceiro numero: ");
+				int num3 = dados.nextInt();
+				
+				System.out.print("Digite o quarto numero: ");
+				int num4 = dados.nextInt();
+				
+				System.out.printf("Você digitou os numeros: " + num1 + " " + num2 + " " + num3 + " " + num4);
+				int result = num1 + num2 + num3 + num4;
+				System.out.printf("\nA soma destes numeros é: " + result);
+				dados.close();
+		}
+}
---
+++ src/matematic/CadastroUser.java
@@ -0,0 +1,26 @@
+package matematic;
+
+import java.util.Scanner;
+
+public class CadastroUser {
+
+
+	public static void main(String[] args) {
+		Scanner dados = new Scanner (System.in); 
+			System.out.print("Digite seu nome: ");
+			String nome = dados.nextLine();
+			
+			System.out.print("Digite sua profissão: ");
+			String profissao = dados.nextLine();
+			
+			System.out.print("Digite sua idade: ");
+			String idade = dados.nextLine();
+			
+			System.out.print("Digite a cidade onde você mora: ");
+			String cidade = dados.nextLine();
+			
+			System.out.println( nome + " é " + profissao + ", tem " + idade + " anos" + " e mora na cidade de " + cidade);
+			dados.close();
+		}
+
+	}
